b1eadedbf509498d652d67a13452ef3089a9b58b
NataniCavalcante <natanicavalcante53@gmail.com>
2022-02-03 09:29:45 -0300

    mudancas em contador

--- src/views/Contador.vue
+++ src/views/Contador.vue
@@ -1,32 +1,49 @@
 <template>
-  <div>
-    <h1> Quantidade de pessoas: {{quantidade}} </h1>
-   <v-btn
-      class="mx-2"
-      fab
-      dark
-      color="green"
-      @click="quantidade += 1">
+<div>
 
+<v-row>
+  <v-col cols= "12">
+     <v-card elevation="14"
+    class="mx-auto"
+    max-width="300"
+    
+  >
+    <v-img
+      src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+      height="200px"
+    ></v-img>
+
+    <v-card-title>
+        Quantidade de pessoas: {{quantidade}}
+    </v-card-title>
+    </v-card>
+  </v-col> 
+</v-row>
+
+<v-row>
+  <v-col cols="6">
+   <v-btn block color= "green"
+      class="mx-2"
+      dark @click="quantidade += 1">
       <v-icon >
         mdi-plus
       </v-icon>
-
     </v-btn>
-  
-     <v-btn
+  </v-col>
+
+
+  <v-col cols="6">
+     <v-btn block color="red"  
     
       class="mx-2"
-      fab
-      dark
-      color="red"
-      @click="quantidade -= 1"
-
+      dark @click="quantidade -= 1"
     >
       <v-icon dark>
         mdi-minus
       </v-icon>
     </v-btn>
+  </v-col>
+</v-row>
   </div> 
   
 </template>
